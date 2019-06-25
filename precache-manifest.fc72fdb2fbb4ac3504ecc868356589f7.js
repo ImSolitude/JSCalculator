e8e953f43c2734481a85eb88685dbc2e0b0b9ade
imsolitude <muhammadcodez@gmail.com>
2019-06-26 00:28:17 +0300

    Updates

---
+++ precache-manifest.fc72fdb2fbb4ac3504ecc868356589f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98f5fc911709a609c6a9be7f28d94cf",
+    "url": "/JSCalculator/index.html"
+  },
+  {
+    "revision": "5b71973daa71c84c999c",
+    "url": "/JSCalculator/static/css/2.b43cd280.chunk.css"
+  },
+  {
+    "revision": "be30cc42d0b13cf90b4b",
+    "url": "/JSCalculator/static/css/main.eb446f6d.chunk.css"
+  },
+  {
+    "revision": "5b71973daa71c84c999c",
+    "url": "/JSCalculator/static/js/2.568d47d8.chunk.js"
+  },
+  {
+    "revision": "be30cc42d0b13cf90b4b",
+    "url": "/JSCalculator/static/js/main.de3b4c1e.chunk.js"
+  },
+  {
+    "revision": "4364284691ffd4d9e90f",
+    "url": "/JSCalculator/static/js/runtime~main.18c636d1.js"
+  }
+]);
